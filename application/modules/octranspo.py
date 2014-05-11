f9e13f237b56a96ca8466f8c41436fa737229d7e
Jacob Rahme <jacob.rahme@gmail.com>
2014-05-11 15:27:44 -0400

    derusting python
    still breaking off the rust. Removed an old and pointless file, added a
    better serialization for the python objects. As well as all of OC
    transpos data files. These will later be transcribed into a DB

--- application/modules/octranspo.py
+++ application/modules/octranspo.py
@@ -1,22 +1,26 @@
 # Oc Transpo getter
 import json
 import requests
+import pickle
 
 class OcTranspoGetter:
 	#import credentials from file, my credentials are not in sample file,
 	#they can be gotten from http://www.octranspo1.com/developers/
 	def __init__(self):
-		credentials = json.loads(open('../config/logins.json').read())[0]
-		json.dumps(credentials)
-
+		credFile = open('../config/logins.ser')
+		credentials = pickle.load(credFile)
 		
-		self.apiKey = credentials['apiLogin']['appId']
-		self.apiPass = credentials['apiLogin']['apiKey']
-		self.payLoad = {'appID':self.apiKey,'apikey':self.apiPass}
+		self.apiID = credentials['apiLogin']['apiID']
+		#self.apiKey = credentials['apiLogin']['apiKey']
+		#self.apiKey = '79e65ca434f89797b63aa0bb4c9ae528'
+		self.payLoad = {'apiID':self.apiID,'apiKey':self.apiKey}
+
+		print self.payLoad
 
 #Get live data
 	def getLiveData(self,bus,stop):
-		soapData = requests.get('https://api.octranspo1.com/v1.2/GetRouteSummaryForStop',params = self.payLoad)
+		self.payLoad['stopNum'] = stop
+		soapData = requests.post('https://api.octranspo1.com/v1.2/GetRouteSummaryForStop',data = self.payLoad)
 		return soapData
 		
 		
--- application/run.py
+++ application/run.py
@@ -1,11 +1,17 @@
 import sys
+import pickle
 sys.path.insert(0, 'modules')
-
 from octranspo import OcTranspoGetter
 import utilities
 
 octranspo = OcTranspoGetter();
-
 soapData = octranspo.getLiveData('3017','94')
 
-print soapToJson(soapData)
+print soapData.text
+
+'''
+credentials = {'database': {'username': 'null', 'host': 'null', 'password': 'null', 'port': 'null'}, 'apiLogin': {'apiID': 'apiID', 'apiKey': 'apikey'}}
+file = open('../config/logins.ser','w');
+pickle.dump(credentials,file)
+file.close()
+'''
