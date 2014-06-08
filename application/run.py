31e8662af22e111d87ad20ba53ec7b6752932719
Jacob Rahme <jacob.rahme@gmail.com>
2014-06-07 20:21:16 -0400

    updating readme
    ignore other commits.

--- application/run.py
+++ application/run.py
@@ -1,19 +1,28 @@
 import sys
 import pickle
+import xml.etree.ElementTree as eTree
 sys.path.insert(0, 'modules')
 from octranspo import OcTranspoGetter
 import utilities
 
-octranspo = OcTranspoGetter();
-soapData = octranspo.getStopData('3017','94')
+octranspo = OcTranspoGetter()
+soapData = octranspo.getStopInfo('118','2954')
 
-print soapData.text
+root =  eTree.fromstring(soapData.text)
 
+for child in root:
+	for grandchild in child:
+		for greatgrandchild in grandchild:
+			print greatgrandchild.tag
+
+for details in root.iter('Routes'):
+	print details.attrib
+	print details.tag
 
 
 
 '''
-credentials = {'database': {'username': 'null', 'host': 'null', 'password': 'null', 'port': 'null'}, 'apiLogin': {'apiID': 'apiID', 'apiKey': 'apikey'}}
+credentit = 'database': {'username': 'null', 'host': 'null', 'password': 'null', 'port': 'null'}, 'apiLogin': {'apiID': 'apiID', 'apiKey': 'apikey'}}
 file = open('../config/logins.ser','w');
 pickle.dump(credentials,file)
 file.close()
